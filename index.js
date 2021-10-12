76a91729ef91c1f5b048f53c56a2908dc04aae3e
Muhammad Al-amin <muhammad.alamindev01@gmail.com>
2021-10-13 00:16:28 +0600

    amazon server

--- index.js
+++ index.js
@@ -2,22 +2,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const MongoClient = require('mongodb').MongoClient;
+const app = express();
+// const {MongoClient} = require('mongodb');
 require('dotenv').config();
 
 
-const app = express();
 app.use(bodyParser.json());
 app.use(cors());
-const port = 5000;
-
-
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.msfnz.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.msfnz.mongodb.net/${process.env.ema-john-store}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-    const collection = client.db("ema-john-store").collection("product");
-    console.log('Database Connected')
-
+    const collection = client.db(`${process.env.ema-john-store}`).collection(`${process.env.DB_COLLECTION}`);
+    // console.log('Database Connected')
     // Database POST
     app.post('/addProduct', (req, res) => {
         const products = req.body;
@@ -30,7 +26,6 @@ client.connect(err => {
             })
     })
 
-
     // Read data from the Server
     app.get('/products', (req, res) => {
         collection.find({})
@@ -39,8 +34,6 @@ client.connect(err => {
             })
     })
 
-
-    //
     // Read data from the Server
     app.get('/product/:key', (req, res) => {
         collection.find({key: req.params.key})
@@ -50,6 +43,7 @@ client.connect(err => {
     })
 });
 
+const port = 5000;
 app.listen(port, () => {
     console.log(`Server is Running http://localhost:${process.env.PORT || port}`)
 })
